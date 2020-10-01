7bd1f35e1b6d062aa8fa8603f6df6dccbf1768b8
Viswanatha Reddy <viswanatha.g15@iiits.in>
2020-10-01 16:52:31 +0530

    Update losses.py

--- losses.py
+++ losses.py
@@ -22,8 +22,9 @@ def calc_iou(a, b):
 
 class FocalLoss(nn.Module):
     #def __init__(self):
-
-    def forward(self, classifications, regressions, anchors, annotations):
+    
+    @staticmethod
+    def forward(classifications, regressions, anchors, annotations):
         alpha = 0.25
         gamma = 2.0
         batch_size = classifications.shape[0]
