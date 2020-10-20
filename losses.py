491a458cbe7ed26be7781db2c87ad688e48b8e2b
Viswanatha Reddy <viswanatha.g15@iiits.in>
2020-10-20 22:49:03 +0530

    Update losses.py

--- losses.py
+++ losses.py
@@ -119,10 +119,8 @@ class FocalLoss(nn.Module):
                 targets = targets/torch.Tensor([[0.1, 0.1, 0.2, 0.2]]).to(device)
                 
                 # negative_indices = ~(positive_indices)
-                '''
-                Use the below line if encountered with an error
-                negative_indices = 1 - positive_indices # For older Pytorch versions
-                '''
+                # negative_indices = 1 - positive_indices # For older Pytorch versions
+                
                 regression_diff = torch.abs(targets - regression[positive_indices, :])
 
                 regression_loss = torch.where(
